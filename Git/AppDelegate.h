d8b30dd6d82746f6c066f44aef1fcc6db2f8edbb
dc004 <dc004@minliping.lan>
2016-04-01 14:04:54 +0800

    Initial Commit

---
+++ Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by dc004 on 16/4/1.
+//  Copyright © 2016年 王珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git
+//
+//  Created by dc004 on 16/4/1.
+//  Copyright © 2016年 王珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
